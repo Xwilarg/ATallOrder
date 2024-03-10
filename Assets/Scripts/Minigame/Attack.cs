e1e5f5579127dd2ec5d92acbdb5ff73fb88b9c16
Ryan <ry_wilmink@hotmail.com>
2024-03-09 21:46:25 -0500

    Combat setups, centaur gif sketches and files

---
+++ Assets/Scripts/Minigame/Attack.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Attack
+{
+    public GameObject AttackPoints; //single prefab with children
+    public int AttackPower;
+}
--- Assets/Scripts/SO/NpcInfo.cs
+++ Assets/Scripts/SO/NpcInfo.cs
@@ -5,5 +5,19 @@ namespace NSFWMiniJam3.SO
     [CreateAssetMenu(menuName = "ScriptableObject/NpcInfo", fileName = "NpcInfo")]
     public class NpcInfo : ScriptableObject
     {
+
+        public string characterName;
+        public Attack[] attackPatterns;
+        public Stats stats;
+
+    }
+
+    [System.Serializable]
+    public struct Stats
+    {
+        public int Health;
+        public float AttackSpeed;
+        public float Defense;
+        public float Power;
     }
 }
