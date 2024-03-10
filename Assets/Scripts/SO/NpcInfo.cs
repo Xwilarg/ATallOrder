9a06498cfdd35333c96d9c38f3c3b5eced0de002
Xwilarg <xwilarg@protonmail.com>
2024-03-10 01:09:03 +0100

    Start prop system

--- Assets/Scripts/SO/NpcInfo.cs
+++ Assets/Scripts/SO/NpcInfo.cs
@@ -5,5 +5,7 @@ namespace NSFWMiniJam3.SO
     [CreateAssetMenu(menuName = "ScriptableObject/NpcInfo", fileName = "NpcInfo")]
     public class NpcInfo : ScriptableObject
     {
+        [SerializeField]
+        private Sprite GameSprite;
     }
 }
---
+++ Assets/Scripts/World/Prop.cs
@@ -0,0 +1,15 @@
+﻿using NSFWMiniJam3.SO;
+using UnityEngine;
+
+namespace NSFWMiniJam3.World
+{
+    public class Prop : MonoBehaviour, IInteractable
+    {
+        public NpcInfo HiddenNpc { set; get; }
+
+        public void Interact(PlayerController pc)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
