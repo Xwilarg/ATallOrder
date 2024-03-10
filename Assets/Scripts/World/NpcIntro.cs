fd83086b2338260cd051a48b51cac26b6611d5df
Xwilarg <xwilarg@protonmail.com>
2024-03-11 00:58:44 +0100

    Add intro NPC

---
+++ Assets/Scripts/World/NpcIntro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace NSFWMiniJam3.World
+{
+    public class NpcIntro : MonoBehaviour, IInteractable
+    {
+        public void Interact(PlayerController pc)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
