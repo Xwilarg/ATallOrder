324c4063f25385b1eda2cd0ffb2ec29dfa830da3
Xwilarg <xwilarg@protonmail.com>
2024-03-10 02:04:14 +0100

    Add asset manager

---
+++ Assets/Scripts/Manager/AssetManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace NSFWMiniJam3.Manager
+{
+    public class AssetManager : MonoBehaviour
+    {
+        public static AssetManager Instance { private set; get; }
+
+        [SerializeField]
+        private GameObject _npcPrefab;
+        public GameObject NpcPrefab => _npcPrefab;
+    }
+}
