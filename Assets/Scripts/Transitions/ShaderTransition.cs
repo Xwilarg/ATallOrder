b4e53d17f2032a497255a217ce276e61a5ac5674
Xwilarg <xwilarg@protonmail.com>
2024-03-10 17:26:05 +0100

    Reset shader value on destroy

--- Assets/Scripts/Transitions/ShaderTransition.cs
+++ Assets/Scripts/Transitions/ShaderTransition.cs
@@ -7,18 +7,23 @@ namespace NSFWMiniJam3.World
         [SerializeField][Range(0, 0.99f)] private float holdBlack = 0.2f;
         [SerializeField] private Material _shader;
 
-        private float _progressMultiplier => 1f / (1 - holdBlack);
+        private float _progressMultiplier => 1f / (1f - holdBlack);
 
         public void OnTransitionProgress(float veilProgress) {
             SetProgress(veilProgress);
         }
 
         public void TransitionEnd() {
-            SetProgress(0);
+            SetProgress(0f);
         }
 
         private void SetProgress(float veilProgress) {
             _shader.SetFloat("_Progress", veilProgress * _progressMultiplier);
+        }
+
+        private void OnDestroy()
+        {
+            SetProgress(0f);
         }
     }
 }
